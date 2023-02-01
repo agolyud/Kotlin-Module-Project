17f4056fa1898cfbca880df1ac4e1e897cee0b6d
Aleksey Golyudov <98583104+agolyd@users.noreply.github.com>
2023-02-01 13:11:44 +0300

    Notes Practicum

---
+++ src/main/kotlin/menu.kt
@@ -0,0 +1,37 @@
+import java.util.*
+
+fun menu(name: String, items: Iterable<String>) {
+    println("$name")
+    println("0. Создать")
+    var quantity = 1
+    items.forEach { println("${quantity++}. $it") }
+    println("${quantity}. Выход")
+}
+
+fun addArchive(archives: MutableMap<String, MutableMap<String, String>>) {
+    println("Введите название архива")
+    val name = input(0)
+    archives.put(name, mutableMapOf())
+}
+
+fun addNote(notes: MutableMap<String, String>){
+    println("Введите название заметки")
+    val name = input(0)
+    notes.put(name, "Введите текст заметки")
+}
+
+fun openNote(notes: MutableMap<String, String>, number: Int){
+    while (true){
+        println(notes.keys.elementAt(number - 1))
+        println(notes.values.elementAt(number - 1))
+        println("0. Выход")
+
+        val textNote = input(0)
+        when(textNote){
+            "0" -> break
+            else ->{
+                notes.put(notes.keys.elementAt(number - 1), textNote)
+            }
+        }
+    }
+}
